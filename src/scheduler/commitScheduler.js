81df19a27f1d0e36aa35c1c6ca6b4502564992e4
Sourav dey <sourav.dey0147@gmail.com>
2025-01-12 12:11:00 +0530

    Daily commit: 2025-01-12

--- src/scheduler/commitScheduler.js
+++ src/scheduler/commitScheduler.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const simpleGit = require("simple-git");
 const fs = require("fs");
 const path = require("path");
 dotenv.config();
 
 // Configuration
-const REPO_PATH = "https://github.com/Sourav19o7/TODO"; // Replace with your repo path
+const REPO_PATH = "."; // Replace with your repo path
 const COMMIT_MESSAGE = "Daily commit: "; // Default commit message
 const GITHUB_USERNAME = "Sourav19o7"; // Replace with your GitHub username
 const GITHUB_TOKEN = process.env.PERSONAL_ACCESS_TOKEN; // Replace with your GitHub token
@@ -14,10 +14,15 @@ const BRANCH_NAME = "main"; // Replace with your branch name if not 'main'
 // Function to make a commit
 async function makeCommit() {
   try {
+
+    if (!fs.existsSync(REPO_PATH)) {
+        throw new Error(`Repository path does not exist: ${REPO_PATH}`);
+      }
+
     const git = simpleGit(REPO_PATH);
 
     // Step 1: Create or update a dummy file
-    const filePath = path.join(REPO_PATH, "src/scheduler/daily_update.txt");
+    const filePath = path.join(REPO_PATH, "/src/scheduler/daily_update.txt");
     const timestamp = new Date().toISOString();
     fs.appendFileSync(filePath, `Update on ${timestamp}\n`);
 
